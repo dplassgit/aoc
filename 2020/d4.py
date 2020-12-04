6033bad904d2177726bf2d6014bfeca8398bf342
David Plass <dplass@gmail.com>
2020-12-04 09:59:29 -0500

    Day 4, part 2

--- 2020/d4.py
+++ 2020/d4.py
@@ -1,27 +1,63 @@
 import io
+import re
 
 f = open("d4.txt", "r")
 
 data=[]
 required=['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-valid=0
+eyes=['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+numvalid=0
 
-passport = []
+passport = {}
 for line in f:
     line=line.strip()
     if len(line) == 0:
         # end of previous passport
         missing = [k for k in required if k not in passport]
-        if not missing or missing == ['cid']:
-            valid = valid + 1
         print missing, passport
-        passport = []
+        if not missing:
+            # no missing fields.
+            valid=True
+            byr = int(passport['byr'])
+            valid = valid and (byr >= 1920 and byr <= 2002)
+            print "valid after byr", valid
+            iyr = int(passport['iyr'])
+            valid = valid and (iyr >= 2010 and iyr <= 2020)
+            print "valid after iyr", valid
+            eyr = int(passport['eyr'])
+            valid = valid and (eyr >= 2020 and eyr <= 2030)
+            print "valid after eyr", valid
+            hgt = passport['hgt']
+            units = hgt[-2:]
+            if units == 'cm':
+                cms = int(hgt[:-2])
+                valid = valid and (cms >= 150 and cms <= 193)
+            elif units == 'in':
+                ins = int(hgt[:-2])
+                valid = valid and (ins >= 59 and ins <= 76)
+            else:
+                valid = False
+            print "valid after hgt", valid
+            hcl = passport['hcl']
+            valid = valid and (re.match('^#[a-f0-9]{6}$', hcl) is not None)
+            print "valid after hcl", valid
+            ecl = passport['ecl']
+            valid = valid and (ecl in eyes)
+            print "valid after ecl", valid
+            pid = passport['pid']
+            valid = valid and (re.match('^[0-9]{9}$', pid) is not None)
+            print "valid after pid", valid
+            if valid:
+                numvalid = numvalid + 1
+
+        passport = {}
     else:
         fields=line.split(' ')
         for field in fields:
             kv=field.split(':')
-            passport.append(kv[0])
+            if kv[0] != 'cid':
+                passport[kv[0]] = kv[1]
         #print line,fields,passport
 
-print valid
+print numvalid
     
