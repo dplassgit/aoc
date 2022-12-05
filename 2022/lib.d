752c1b394bc525a025d9af49622387c186893493
David Plass <dplassgit@users.noreply.github.com>
2022-12-05 14:53:54 -0500

    Implemented basic singly-linked list.

--- 2022/lib.d
+++ 2022/lib.d
@@ -51,6 +51,76 @@ split: proc(s:string, div:string): string[] {
   return parts
 }
 
+
+DValue:record { // your data here
+  value:string
+}
+DEntry:record { value: DValue next:DEntry}
+DList:record { head:DEntry }
+
+push: proc(list: DList, value: DValue): DList {
+  e = new DEntry
+  e.value = value
+  e.next = list.head
+  list.head = e
+  return list
+}
+
+append: proc(list: DList, value: DValue): DList {
+  // 1. Find tail
+  // 2. Add new tail
+  newtail = new DEntry
+  newtail.value = value
+  if list.head == null {
+    // no tail
+    list.head = newtail
+    return list
+  }
+  tail = list.head while tail.next != null do tail = tail.next {
+  }
+  // tail is really the tail
+  tail.next = newtail
+  return list
+}
+
+pop: proc(list: DList): DValue {
+  if list.head == null {
+    exit "head of list is null"
+  }
+  value = list.head.value
+  list.head = list.head.next
+  return value
+}
+
+head: proc(list: DList): DValue {
+  if list.head == null {
+    exit "head of list is null"
+  }
+  return list.head.value
+}
+
+printList: proc(list: DList) {
+  tail = list.head while tail != null do tail = tail.next {
+    println tail.value.value
+  }
+}
+
+makeDValue: proc(v:string):DValue {
+  dval = new DValue
+  dval.value = v
+  return dval
+}
+
+//list = new DList
+//append(list, makeDValue("hi"))
+//printList(list) println "-"
+//push(list, makeDValue("first"))
+//printList(list) println "-"
+//append(list, makeDValue("bye"))
+//printList(list) println "-"
+//pop(list)
+//printList(list) println "-"
+
 //println "splitting a,b,c:" println split("a,b,c", ",")
 //println "splitting a,b,:" println split("a,b,", ",")
 //println "splitting a:" println split("a", ",")
