6656d30cc2a4ba06f05706f95db251823c932f81
David Plass <dplassgit@users.noreply.github.com>
2022-12-08 18:43:14 -0500

    Day 8 part 2finafreakingly.

--- 2022/day8.d
+++ 2022/day8.d
@@ -45,10 +45,10 @@ parse_row: proc(row:int, line:string) {
 }
 
 height=0
+part1=0
 reset_next_line()
 line = next_line() while line != null do line = next_line() {
   parse_row(height, line)
-  println line
   height = height + 1
 }
 
@@ -60,20 +60,27 @@ xy: proc(x:int, y:int):int {
 seen:bool[height * width]
 i = 0 while i < width do i = i + 1 {
   // the whole first row
-  seen[i] = true
+  setSeen(i, 0)
   // the whole last row
-  seen[i+(height-1)*height] = true
+  setSeen(i, height-1)
 }
 i = 0 while i < height do i = i + 1 {
   // the whole first column
-  seen[i*height] = true
+  setSeen(0, i)
   // the whole last column
-  seen[i*height+width-1] = true
+  setSeen(width-1, i)
+}
+
+setSeen:proc(x:int, y:int) {
+  if not seen[x+y*height] {
+    seen[x+y*height] = true
+    part1 = part1 + 1
+  }
 }
 
 print_seen: proc() {
   i = 0 while i < height do i = i + 1 {
-    j = 0 while j < height do j = j + 1 {
+    j = 0 while j < width do j = j + 1 {
       print seen[i*height + j] print ","
     }
     println ""
@@ -82,28 +89,17 @@ print_seen: proc() {
 //print_seen()
 //println ""
 
-count_seen: proc():int {
-  answer = 0
-  i = 0 while i < height do i = i + 1 {
-    j = 0 while j < height do j = j + 1 {
-      if seen[i*height + j] {answer = answer + 1}
-    }
-  }
-  return answer
-}
 
 process_swath: proc(sx: int, sy:int, ex: int, ey: int, dx:int, dy:int) {
   tallest = xy(sx, sy)
-//  print "initial tallest " println tallest
   if sx == ex {
     // one column
     x = sx
     y = sy while y != ey do y = y + dy {
       it = xy(x, y)
- //     print x print "," print y print ":" println it
       if it > tallest {
         tallest = it
-        seen[x+y*height] = true
+        setSeen(x, y)
       }
     }
   } else {
@@ -111,10 +107,9 @@ process_swath: proc(sx: int, sy:int, ex: int, ey: int, dx:int, dy:int) {
     y = sy
     x = sx while x != ex do x = x + dx {
       it = xy(x, y)
-  //    print x print "," print y print ":" println it
       if it > tallest {
         tallest = it
-        seen[x+y*height] = true
+        setSeen(x, y)
       }
     }
   }
@@ -135,6 +130,45 @@ x = 1 while x < width - 1 do x = x + 1 {
   // look at column x from bottom to top
   process_swath(x, height-1, x, 0,      0, -1)
 }
-//print_seen()
+// print_seen()
 
-println count_seen()
+print 'Part1: ' println part1
+
+// OH I THINK I TSEE IT NOW
+// 3 cases: 1 off the grid: stop. 2. not off the grid, i'm bigger: keep going. 3. not off the grid, they're bigger: stop.
+good: proc(x:int, y:int, me:int): int {
+  if x < 0 or y < 0 { return 0 }
+  if x >= width or y >= height { return 0 }
+  if me > xy(x, y) { return 1} else {return -1}
+}
+
+calcScenicHeight: proc(x: int, y:int): int {
+  me = xy(x, y)
+  // go up down left right, get length, multiple
+  left = 0
+  right = 0
+  up = 0
+  down = 0
+  tx = x - 1 while true { g = good (tx, y, me) if g == 0 { break} else { left = left + 1 tx = tx - 1 if g == -1 { break }}}
+  tx = x + 1 while true { g = good (tx, y, me) if g == 0 { break} else { right = right + 1 tx = tx + 1 if g == -1 { break }}}
+  ty = y - 1 while true { g = good (x, ty, me) if g == 0 { break} else { up = up + 1 ty = ty - 1 if g == -1 { break }}}
+  ty = y + 1 while true { g = good (x, ty, me) if g == 0 { break} else { down = down + 1 ty = ty + 1 if g == -1 { break }}}
+
+  return left*right*up*down
+}
+
+// for part 2:
+part2 = -1
+y = 1 while y < height - 1 do y = y + 1 {
+  x = 1 while x < width - 1 do x = x + 1 {
+    // scenic height
+    sh = calcScenicHeight(x, y)
+    if sh > part2 {
+  print sh print " is best at " print x print "," println y
+      part2 = sh
+    }
+  }
+}
+
+
+print 'Part2: ' println part2
