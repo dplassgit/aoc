be12c5d658a3dc760110d7556d65ade6305b39e7
David Plass <dplassgit@users.noreply.github.com>
2022-12-09 09:25:54 -0500

    Day 9 in python (does not read from file yet)

---
+++ 2022/day9.py
@@ -0,0 +1,93 @@
+input=[
+'R 4',
+'U 4',
+'L 3',
+'D 1',
+'R 4',
+'D 1',
+'L 5',
+'R 2']
+
+
+# for visualization only
+left=-10 
+right=10 
+top=10 
+bottom=-10
+head=[0,0]
+last_head=head
+global tail
+tail=[0,0]
+Y=0
+X=1
+
+tailspots = set()
+def recordtail():
+    tailspots.add((tail[X], tail[Y]))
+recordtail()
+
+def xdist():
+    # is this right?
+    return abs(head[X]-tail[X])
+
+def ydist():
+    # is this right?
+    return abs(head[Y]-tail[Y])
+
+def needToMoveTail():
+    # if they're at the same spot, it's fine
+    # if they're touching it's fine
+    if head == tail: return False
+    # print("Not the same place")
+    if xdist() <= 1 and ydist() <= 1: return False
+    # print("xdist %d ydist %d" % (xdist(), ydist()))
+    return True
+
+def maybeMoveTail():
+    global tail
+    print("head now at %s" % head)
+    print("tail was at %s" % tail)
+    if needToMoveTail():
+        print("moving tail to last head %s" % last_head)
+        tail=last_head[:]
+        recordtail()
+
+
+def visualize():
+    print()
+    for y in range(top, bottom, -1):
+        for x in range(left, right):
+            if head[X] == x and head[Y] == y: print("H",end='')
+            elif tail[X]==x and tail[Y] == y: print("T",end='')
+            else: print(".",end='')
+        print()
+
+visualize()
+for line in input:
+    print(line)
+    (dir, count)= line.split(' ')
+    count = int(count)
+    if dir == 'U':
+        for i in range(0, count):
+            last_head = head[:]
+            head[Y] = head[Y] + 1
+            maybeMoveTail()
+    elif dir == 'D':
+        for i in range(0, count):
+            last_head = head[:]
+            head[Y] = head[Y] - 1
+            maybeMoveTail()
+    elif dir == 'L':
+        for i in range(0, count):
+            last_head = head[:]
+            head[X] = head[X] - 1
+            maybeMoveTail()
+    elif dir == 'R':
+        for i in range(0, count):
+            last_head = head[:]
+            head[X] = head[X] + 1
+            maybeMoveTail()
+    visualize()
+
+print("Part1: %d" % len(tailspots))
+print(tailspots)
