ff24931bb528a825b2d20f45d7613e252cf53031
David Plass <dplassgit@users.noreply.github.com>
2023-12-09 11:50:31 -0500

    Day 9 done and done. "Is that it?!"

---
+++ 2023/day9b.py
@@ -0,0 +1,36 @@
+f = open('day9.txt', 'r')
+lines = f.readlines()
+lines = [line.strip() for line in lines]
+
+def all_zeros(lst):
+    return len([x for x in lst if x != 0]) == 0
+
+answer = 0
+for line in lines:
+    parts = [int(x) for x in line.split()]
+    #print(parts)
+    iters=[parts]
+    olddiffs = parts
+    while True:
+        diffs = [0]*(len(olddiffs)-1)
+        for i in range(1, len(olddiffs)):
+            diffs[i-1] = olddiffs[i] - olddiffs[i-1]
+        iters.insert(0, diffs)
+        if all_zeros(diffs):
+            break
+        olddiffs = diffs
+    #print(iters)
+    # now rebuild the thing
+
+    print(iters)
+    p = iters[0][-1]# prediction
+    for i in range(1, len(iters)):
+        lst = iters[i] # this is the next row
+        mylast = lst[0] # the last one in our row
+        p = mylast - p # add the last one in our row to the last prediction (?)
+        print("new prediction %d" % p)
+
+    answer += p
+
+print("Day 9 part 2: %d" % answer)
+
